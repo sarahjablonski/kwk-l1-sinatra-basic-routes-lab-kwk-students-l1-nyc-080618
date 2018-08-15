c56fe6730c830ce9d10f9cbd483d765b3304ea10
sarahjablonski <shapeshiftwithme@gmail.com>
2018-08-15 15:49:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/name" do
+    "Hello my name is Sarah"
+    end
+    
+  get "/hometown" do
+    "My hometown is New York"
+  end
+  
+  get "/favorite-song" do
+    "My favorite song is Bohemian Rhapsody"
+  end
+  
+  
 end
